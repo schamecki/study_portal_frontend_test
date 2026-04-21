e2b7cb4a4136e40c57d56166005c2ecb1f8e160e
schameck <tameuarnaudschameck0@gmail.com>
2026-04-21 07:43:34 +0100

    final commit

--- src/config/pageTitle.ts
+++ src/config/pageTitle.ts
@@ -7,7 +7,7 @@ interface PageTitles {
 
 export const PAGE_TITLES: PageTitles = {
     "/": {name: 'accueil', title: "Accueil - Mon Application"},
-    "/profil": {name: 'profile', title: "Profile - Mon Application"},
+    "/profile": {name: 'profil', title: "Profile - Mon Application"},
     "/avi": {name: 'Obtenir mon A.V.I', title: "Obtenir mon A.V.I - Boaz Study"},
     "/avi/:id": {name: 'Obtenir mon A.V.I', title: (id: string) => `Étape ${id} - A.V.I - Boaz Study`},
 }
